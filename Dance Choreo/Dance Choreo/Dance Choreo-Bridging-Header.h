e20d8a48e4b45a488164a44407cd1f761916ac66
quangcito <qnguyen2@macalester.edu>
2022-05-16 23:07:01 +0700

    Setting up the SDK
    - imported Spotify iOS framework
    - configured info.plist
    - Set -ObjC Linker Flag
    - add bridging header

---
+++ Dance Choreo/Dance Choreo/Dance Choreo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Dance Choreo-Bridging-Header.h
+//  Dance Choreo
+//
+//  Created by Nguyễn Đương Quang on 5/16/22.
+//
+
+#ifndef Dance_Choreo_Bridging_Header_h
+#define Dance_Choreo_Bridging_Header_h
+
+
+#endif /* Dance_Choreo_Bridging_Header_h */
+
+#import <SpotifyiOS/SpotifyiOS.h>
